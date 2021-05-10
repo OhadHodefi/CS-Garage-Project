2901c828925c60417bab6f0f68e3b6cedbe5f768
Mosko <syao66@gmail.com>
2021-05-10 20:40:42 +0300

    UI updates

---
+++ Ex03.ConsoleUI/Messages.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+
+namespace Ex03.ConsoleUI
+{
+    static public class Messages
+    {
+        internal enum eMenuQueries
+        {
+            AddVehicle = 1,
+            ViewLicenseNumbers,
+            ChangeState,
+            InflateTires,
+            FuelVehicle,
+            ChargeVehicle,
+            ViewInfoByLicense,
+            Exit
+        }
+
+        internal static readonly string[] k_MenuMessages =
+        {
+            @"===================
+Garage Manager 2021
+===================
+",
+            @"{0}. Add a new vehicle to the garage
+",
+            @"{0}. View license numbers of vehicles currently in the garge
+",
+            @"{0}. Change vehicle state
+",
+            @"{0}. Fully inflate vehicle tires
+",
+            @"{0}. Add fuel to a vehicle
+",
+            @"{0}. Charge a vehicle
+",
+            @"{0}. View information of a vehicle
+",
+            @"{0}. Exit
+"
+        };
+
+        public static string GetMenuMessage(int i_MenuChoice)
+        {
+            return k_MenuMessages[i_MenuChoice];
+        }
+
+        internal enum eInputQueries
+        {
+            VehicleInfo,
+            State,
+            LicenseNumber,
+            Energy,
+            WheelPressure,
+            PersonalInfo,
+            Name,
+            PhoneNumber,
+            GasType,
+            GasAmount,
+            ChargeTime,
+        }
+
+        internal static readonly string[] k_InputMessages =
+        {
+            "vehicle details",
+            "vehicle state",
+            "vehicle's license number",
+            "current amount of energy (gas/electricity left)",
+            "current wheels pressure",
+            "personal info",
+            "name",
+            "phone number",
+            "type of gas",
+            "amount to fill",
+            "minutes to charge"
+        };
+    }
+}
--- Ex03.ConsoleUI/Program.cs
+++ Ex03.ConsoleUI/Program.cs
@@ -8,6 +8,9 @@ namespace Ex03.ConsoleUI
 {
     class Program
     {
-
+        public static void Main()
+        {
+            ConsoleUI.Run();
+        }
     }
 }
